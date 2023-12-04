11dffe5a5b080ce9a4c8accbea92ebb46978623d
fairuuz16 <firuz.firaz@gmail.com>
2023-12-04 16:39:25 +0700

    add: ilustration algo

--- algo.py
+++ algo.py
@@ -240,12 +240,14 @@ for node, neighbors in g.graph.items():
         G.add_edge(node, neighbor, weight=g.edge_costs.get((node, neighbor, 0)))
 
 pos = nx.circular_layout(G)
+# pos = nx.spring_layout(G)
 
 node_labels = {node: f"{node}\nH: {g.static_heuristic.get(node, 0)}" for node in G.nodes}
 edge_labels = {(node, neighbor): g.edge_costs.get((node, neighbor), 0) for node, neighbor in G.edges}
 
-# nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=700, node_color="skyblue", font_size=8)
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+plt.figure(figsize=(15, 10))
+nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=700, node_color="skyblue", font_size=8)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
 
-# # Tampilkan gambar
-# plt.show()
+# Tampilkan gambar
+plt.show()

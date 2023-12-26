8435e7cad7800751cf57a4186ec4fc4ad99d6ca3
fairuuz16 <firuz.firaz@gmail.com>
2023-12-26 09:35:04 +0700

    finalize algorithm

--- algo.py
+++ algo.py
@@ -1,27 +1,38 @@
+# SOURCE CODE FINAL PROJECT 
+# KELOMPOK 15 KKA(C)
+# Nama: 1. Fairuuz Azmi Firas - 5025221057
+#       2. Ivan Fairuz Adinata - 5025221167
+#       3. Gilang Kista Ramadhan - 502522173
+
+# Import library yang dibutuhkan
 import heapq
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# Definisikan kelas Graph
 class Graph:
     def __init__(self):
-        # Class constructor and its attributes
+        # Konstruktor kelas dan atribut-atributnya
         self.graph = defaultdict(list)
         self.static_heuristic = {}
         self.edge_costs = {}
         
+    # Metode untuk menambahkan edge ke dalam graf
     def add_edge(self, from_node, to_node, cost):
-        # Add edges with a certain cost to the graph
+        # Menambahkan edge dari from_node ke to_node dengan cost tertentu
         self.graph.setdefault(from_node, []).append(to_node)
         self.edge_costs[(from_node, to_node)] = cost
 
+    # Metode untuk menambahkan heuristic statis untuk suatu node
     def add_static_heuristic(self, node, heuristic_value):
-        # Add static heuristics for a node
+        # Menambahkan heuristic statis untuk suatu node pada graf
         self.static_heuristic[node] = heuristic_value
 
+    # Metode implementasi algoritma A* search untuk mencari jalur terpendek untuk suatu node pada graf
     def a_star_search(self, start, finish):
-        # Implementation of A* search algorithm
+        # Implementasi algoritma A* search
         visited = set()
         priority_queue = [(0, start)]
         g_values = {node: float('inf') for node in self.graph.keys()}
@@ -56,8 +67,10 @@ class Graph:
 
         return None
 
+# Inisialisasi objek Graph
 g = Graph()
 
+# Definisi jalur utara pada graf
 # JALUR UTARA
 g.add_edge('SURABAYA PASAR TURI', 'LAMONGAN', 38)
 g.add_edge('LAMONGAN', 'BOJONEGORO', 62)
@@ -83,6 +96,7 @@ g.add_edge('JATIBARANG', 'CIREBON', 40)
 g.add_edge('CIKAMPEK', 'JATIBARANG', 98)
 g.add_edge('KARAWANG', 'CIKAMPEK', 22)
 
+# Definisi jalur tengah pada graf
 # JALUR TENGAH
 g.add_edge('SURABAYA GUBENG', 'SEPANJANG', 45)
 g.add_edge('SEPANJANG',	'MOJOKERTO', 45)
@@ -118,6 +132,7 @@ g.add_edge('JATIBARANG', 'CIREBON', 40)
 g.add_edge('CIKAMPEK', 'JATIBARANG', 98)
 g.add_edge('KARAWANG', 'CIKAMPEK', 22)
 
+# Definisi jalur selatan pada graf
 # JALUR SELATAN
 g.add_edge('SURABAYA GUBENG', 'SEPANJANG', 45)
 g.add_edge('SEPANJANG',	'MOJOKERTO', 23)
@@ -155,7 +170,7 @@ g.add_edge('CIPEUNDEUY', 'TASIKMALAYA', 33)
 g.add_edge('NAGREG', 'CIPEUNDEUY', 43)
 g.add_edge('PADALARANG', 'NAGREG', 45)
 
-
+# Definisi jalur tambahan pada graf
 # JALUR TAMBAHAN
 g.add_edge('HALIM',	'PASAR SENEN',	9)
 g.add_edge('PADALARANG', 'TEGALUAR', 28)
@@ -165,6 +180,7 @@ g.add_edge('PASAR SENEN', 'HALIM', 9)
 g.add_edge('TEGALUAR', 'PADALARANG', 28)
 g.add_edge('SURABAYA GUBENG', 'SURABAYA PASAR TURI', 5)
 
+# Definisi heuristic statis untuk stasiun pada graf
 # HEURISTIK STASIUN 
 g.add_static_heuristic('SURABAYA PASAR TURI', 35)
 g.add_static_heuristic('LAMONGAN', 60)
@@ -221,12 +237,36 @@ g.add_static_heuristic('PASAR SENEN', 35)
 g.add_static_heuristic('PADALARANG', 50)
 g.add_static_heuristic('TEGALUAR', 45)
 
+# Tentukan stasiun awal dan akhir untuk hasil yang statis
 start_node = 'PADALARANG'
 finish_node = 'SURABAYA PASAR TURI'
 
+# Lakukan A* search dan tampilkan hasilnya
 result = g.a_star_search(start_node, finish_node)
 
 if result:
     print(f"Jalur dari {start_node} ke {finish_node}: {' -> '.join(result)}")
 else:
     print(f"Tidak ada jalur dari {start_node} ke {finish_node}")
+
+
+#########################################
+
+G = nx.DiGraph()
+
+for node, neighbors in g.graph.items():
+    for neighbor in neighbors:
+        G.add_edge(node, neighbor, weight=g.edge_costs.get((node, neighbor, 0)))
+
+pos = nx.random_layout(G)
+# pos = nx.circular_layout(G)
+
+node_labels = {node: f"{node}\nH: {g.static_heuristic.get(node, 0)}" for node in G.nodes}
+edge_labels = {(node, neighbor): g.edge_costs.get((node, neighbor), 0) for node, neighbor in G.edges}
+
+plt.figure(figsize=(10, 5))
+nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=700, node_color="skyblue", font_size=8)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+
+# Tampilkan gambar
+plt.show()
